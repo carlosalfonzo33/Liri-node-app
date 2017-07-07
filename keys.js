bfe6c1321b13f82020704c6175765250bc47e64e
carlosalfonzo33 <carlos.alfonzo@gmail.com>
2017-07-06 18:05:55 -0700

    made changes to keys.js file

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 exports.twitterKeys = {
   consumer_key: 'XETS5hgMARaqUsjd3C14itM4y',
   consumer_secret: 'K0WJk5Tek4hJvwd5Zf6DDU7bvziyYeWLHoMC4NIBrm5seB5IVK',
-  access_token_key: ' 621439132-uIDxut5g8gGzmTmRAIYpZg5W7sL5hmV63NXqFWSC',
-  access_token_secret: ' X9vgBITu2EflomPPfKHrXR38ASjv19O0HLUkYQ28sRXJB',
+  access_token_key: '621439132-uIDxut5g8gGzmTmRAIYpZg5W7sL5hmV63NXqFWSC',
+  access_token_secret: 'X9vgBITu2EflomPPfKHrXR38ASjv19O0HLUkYQ28sRXJB',
 }
---
+++ liri.js
@@ -0,0 +1,30 @@
+var keys = require("./keys.js");
+var twitter = require("twitter");
+var fs = require("fs");
+var liriCommand = process.argv[2];
+var twitterKeys = keys.twitterKeys;
+
+var twitterUser = new twitter({
+    consumer_key: twitterKeys.consumer_key,
+    consumer_secret: twitterKeys.consumer_secret,
+    access_token_key: twitterKeys.access_token_key,
+    access_token_secret: twitterKeys.access_token_secret
+});
+
+console.log(twitterUser.consumer_key);    
+
+switch(liriCommand) {
+    case "my-tweets":
+    myTweets();
+    break;
+}
+
+function myTweets(){
+    twitterUser.get('search/tweets', {q: 'carlosalfonzo33', count: 20}, function(error, tweet, response) {
+        if(error){
+            console.log(error);
+        }else{
+            console.log(tweet);
+        }
+    });
+};
